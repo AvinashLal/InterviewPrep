5d8b1de38c523a637ce6f462217267f0ea5b5248
Avinash Lal <lal@college.harvard.edu>
2018-08-11 11:53:13 -0400

    added files

---
+++ OOP/testing.py
@@ -0,0 +1,3 @@
+counter = 0
+x = [[counter for r in xrange(2)] for x in xrange(2) counter += 1]
+print x
---
+++ OOP/tictactoe.py
@@ -0,0 +1,44 @@
+class tictactoe(object):
+	def __init__ (self):
+		self.game_over = False
+		self.move_count = 0 
+		self.winner = 0
+		self.board = self.empty_board()
+		self.player_1_spots = []
+		self.player_2_spots = []
+
+	def player_move(self, player, row, col):
+		if player == 1:
+			if self.board[row][col] == ' ' :
+				self.board[row][col] = 'X'
+				self.player_1_spots.append(two_to_one_d_position(row,col)).sort()  
+				self.move_count += 1 
+				return
+			else:
+				print('Already occupied')
+
+	def empty_board(self):
+		self.ROWS = 3
+		self.COLS = 3 
+		[[0 for r in xrange(self.ROWS)] for c in xrange(self.COLS)]
+
+	def two_to_one_d_position(self, tuple):
+		r,c = tuple
+		return (3 * r) + c + 1
+
+	def __str__ (self):
+		output = ''
+		for row in range(self.ROWS):
+			output = output + '|'.join(self.board[row])  + '\n' + '-|-|-' + '\n'
+		return output
+ttt = tictactoe()
+print str(ttt)
+
+def playgame():
+	#create board
+	ttt = tictactoe()
+	print str(ttt)
+	#alternate player move input
+	player_1_move = raw_input('Player 1 choose your spot')
+	#rerender board
+	player_2_move = raw_input('Player 2 choose your spot')
