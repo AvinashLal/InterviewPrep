a96f34202f765c049662fa49cc4ffcd20ef12ddb
Avinash Lal <lal@college.harvard.edu>
2018-08-13 12:40:40 -0400

    functional tictactoe

--- OOP/testing.py
+++
@@ -1,3 +0,0 @@
-for i in range(self.size):
-	n + str(i) = 1
-	print n + str(i)
--- OOP/tictactoe.py
+++ OOP/tictactoe.py
@@ -1,54 +1,78 @@
-from linkedlist import linkedlist, node
 
 class tictactoe(object):
 	def __init__ (self):
 		self.ROWS = 3
 		self.COLS = 3 
-		self.size = self.ROWS*self.COLS
-		self.game_over = False
-		self.move_count = 0 
+		self.move_count = 0
 		self.winner = 0
-		self.board = self.empty_board()
+		self.game_over = False
+		self.valid_move = False
 		self.player_1_spots = []
 		self.player_2_spots = []
-		for i in range(self.size):
-			n + str(i) = 
+		self.board = [[str(0) for r in xrange(self.ROWS)] for c in xrange(self.COLS)]
+		self.three_in_a_row = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9]]
+
 	def player_move(self, player, row, col):
-		if player == 1:
-			if self.board[row][col] == ' ' :
+		row -= 1
+		col -= 1
+		if self.board[row][col] == '0':
+			if player == 1:
 				self.board[row][col] = 'X'
-				self.player_1_spots.append(two_to_one_d_position(row,col)).sort()  
-				self.move_count += 1 
-				return
+				if player == 1:
+					self.player_1_spots.append(self.two_to_one_d_position(row,col))
+					self.player_1_spots.sort()  
+					self.move_count += 1 
+					return
 			else:
-				print('Already occupied')
+				self.board[row][col] = 'O'	
+				if player == 1:
+					self.player_1_spots.append(self.two_to_one_d_position(row,col))
+					self.player_1_spots.sort()  
+					self.move_count += 1 
+					return
+			self.valid_move = True
+		else:
+			print('Already occupied') 
 
-	def empty_board(self):
-		[[0 for r in xrange(self.ROWS)] for c in xrange(self.COLS)]
+	def two_to_one_d_position(self, r, c):
+		return (3 * r) + c + 1
 
-	def two_to_one_d_position(self, tuple):
-		r,c = tuple
-		return (3 * r) + c 
+	def check_winner(self, player):
+		if player == 1:
+			player_spots = self.player_1_spots
+		else:
+			player_spots = self.player_2_spots		
+		number_of_spots = len(player_spots)
+		for i in range (0,len(player_spots) - 2):
+				three_spots = [player_spots[i], player_spots[i+1], player_spots[i+2]]
+				#print three_spot
+				if three_spots in self.three_in_a_row:
+						self.winner = player
+						self.game_over = True
+						return 
 
-	# def check_winner(self, player_spots):
-	# 	counter = 0
-	# 	while counter > 3:
-	# 		if spot == 0:
-	# 		elif spot == 1:
-	# 		elif spot == 1:
-	# def __str__ (self):
-	# 	output = ''
-	# 	for row in range(self.ROWS):
-	# 		output = output + '|'.join(self.board[row])  + '\n' + '-|-|-' + '\n'
-	# 	return output
-# ttt = tictactoe()
-# print str(ttt)
+	def __str__ (self):
+		output = ''
+		for row in range(self.ROWS):
+			output = output + '|'.join(self.board[row])  + '\n' + '-|-|-' + '\n'
+		return output
 
-# def playgame():
-# 	#create board
-# 	ttt = tictactoe()
-# 	print str(ttt)
-# 	#alternate player move input
-# 	player_1_move = raw_input('Player 1 choose your spot')
-# 	#rerender board
-# 	player_2_move = raw_input('Player 2 choose your spot')
+def playgame():
+	#create board
+	ttt = tictactoe()
+	print str(ttt)
+	player_alternater = 1
+	while not ttt.game_over: 
+		ttt.valid_move = False
+		while not ttt.valid_move:
+			#alternate player move input
+			player_move = raw_input('Player ' + str(player_alternater) + ' choose your spot ')
+			x, y = int(player_move[0]), int(player_move[2])
+			ttt.player_move(player_alternater, x, y)
+			print str(ttt)
+		if player_alternater == 1:
+			player_alternater = 2
+		elif player_alternater == 2:
+			player_alternater = 1
+
+playgame()
